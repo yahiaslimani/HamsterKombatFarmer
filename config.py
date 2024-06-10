9d9a83740fd03160e20868e9ae5c4cb5ec6381a1
Yahia SLIMANI <ey_slimani@esi.dz>
2024-06-10 19:20:37 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 
 TOKENS = [
+    {'name': 'account1', "token": "1718042291423bxmUxsx5IyXdRuPF9BzEagkyviAAZ6xEhIRFgN9mn6yScpqV0IQJuFVhBql6dxx71893670855"},
     {'name': 'account1', "token": "171800839197950GzFaxlspkDKwGi6CBvLOXFo58bmHnLqzytcYUvjDboziVKdkFgUFyJYyrd1Z4i7041881839"},
 ]
